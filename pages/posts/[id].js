e2000900cc772ab6eda877304ed644993874e4d5
dikshadhote <diksha.dhote@galepartners.com>
2023-04-17 15:04:42 +0530

    [Fix]:Import error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
-import { useRouter } from "next/Router";
+import { useRouter } from "next/router";
 
 export default function Post({ postData }) {
   const router = useRouter();
